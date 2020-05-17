f1d9dc14ba85f36a08a9116b2e08dbbad720e46d
georgiafrancisco <61526576+georgiafrancisco@users.noreply.github.com>
2020-05-17 20:52:55 +0800

    Update rectangle.sh

--- portfolio2/rectangle.sh
+++ portfolio2/rectangle.sh
@@ -5,7 +5,7 @@
 
 #creating a title
 
-echo 'Rectangles'
+echo 'Rectangle Dimensions'
 echo '   '
 
 #open and display the content of rectangle.txt
@@ -14,8 +14,13 @@ cat rectangle.txt
 
 echo '   '
 
-#formatting the context of rectangle.txt using 'sed'
-#sending the 'sed' formatted context of rectangle.txt to be outpputted into rectangle_f.txt
+#message for indicating that the output of the 'sed' formatted data has been put into rectangle_f.txt
+
+echo 'This content of rectangle.txt has been altered and transferred to rectangle_f.txt'
+echo '   '
+
+#formatting the content of rectangle.txt using 'sed'
+#sending the 'sed' formatted content of rectangle.txt to be outputted into rectangle_f.txt
 
 sed -e 's/ /,/g' rectangle.txt | sed -e 's/Rec/Name:Rec/g' | sed -e 's/,/ Height:/' |
 sed -e 's/,/ Width:/' | sed -e 's/,/ Area:/' | sed -e 's/,/ Colour:/' >> rectangle_f.txt
